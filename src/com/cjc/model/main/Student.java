70eb0d3ae2614d1674f63e1672cf74b026de457d
MaheshMasne <maheshmasne88@gmail.com>
2024-11-17 19:38:47 +0530

    Committing  to branch1

--- src/com/cjc/model/main/Student.java
+++ src/com/cjc/model/main/Student.java
@@ -3,6 +3,7 @@ package com.cjc.model.main;
 public class Student {
 public static void main(String[] args) {
 	System.out.println("I am in Student class");
+System.out.println("My name is Ruchira");
 }
 	
 }
